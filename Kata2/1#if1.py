15357d83c39fcb27f53c17a6e5370567476b86e0
Pavel <pavelramirez@outlook.com>
2020-05-16 12:21:02 +0200

    Cambios 2

---
+++ Kata2/1#if1.py
@@ -0,0 +1,8 @@
+password = "contraseña"
+
+password_del_usuario = input ("introduzca contraseña: ")
+
+if password == password_del_usuario:
+    print("el password es correcto")
+else:
+    print("el password no coincide")
