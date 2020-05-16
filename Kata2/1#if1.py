1eba3a32fc0651742c30e895b8b423b3044d16e5
Pavel <pavelramirez@outlook.com>
2020-05-16 12:26:09 +0200

    Cambio 2

--- Kata2/1#if1.py
+++ Kata2/1#if1.py
@@ -1,6 +1,7 @@
 password = "contraseña"
 
 password_del_usuario = input ("introduzca contraseña: ")
+password_del_usuario=password_del_usuario.lower()
 
 if password == password_del_usuario:
     print("el password es correcto")
